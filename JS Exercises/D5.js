79a23f3cde819c0cbcc4a55b1060d575c58e5201
Martino Brundu <martino.brundu@libero.it>
2024-10-11 17:14:28 +0200

    finish

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -81,6 +81,10 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
 
+while ( ) {
+  
+}
+
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
